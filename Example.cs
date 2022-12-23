bbd3747c36bfe7ff30fff140a62a7c78d3dfa33f
tdqctdqc <tynangerald@gmail.com>
2022-12-22 23:58:31 -0500

    added explanation comments to example

--- Example.cs
+++ Example.cs
@@ -2,6 +2,10 @@ using System;
 
 public class Example : IStaticAccess<int>
 {
+    //two styles of accessing 
+    //first is for when you want to set the member by reflection 
+    //second is for when you want implementing class to set member
+    
     public int Member1 
         => IStaticAccess<int>.MemberAccessor.Get<Game>();
     public int Member2 
